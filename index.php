dc57ed8af1d081aeddcddb9ace59e33df149a7cc
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-26 14:49:56 +0100

    Category class created

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
         private $name;
         private $price;
         private $weight;
+        private Category $category;
 
-        public function __construct ($name, $price, $weight){
+        public function __construct ($name, $price, $weight, $category){
 
             $this -> setName($name);
             $this -> setPrice($price);
             $this -> setWeight($weight);
+            $this -> setCategory($category);
         }
 
         public function getName(){
@@ -42,15 +44,51 @@
             $this -> weight = $weight;
         }
 
+        public function getCategory(){
+
+            return $this -> category; 
+        }
+        public function setCategory($category){
+
+            $this -> category = $category;
+        }
+
         public function getHtml(){
 
             return $this -> getName() . ' '
                     . $this -> getPrice() . ' euro '
-                    . $this -> getWeight() . ' kg';
+                    . $this -> getWeight() . ' kg'
+                    . $this -> $category -> getHtml();
         }
     }
 
-    $product = new Product ('Osso finto' , 180, 500);
+    class Category {
+
+        private $category;
+
+        public function __construct($category){
+
+            $this -> setCategory($category);
+        }
+
+        public function getCategory(){
+
+            return $this -> category;
+        }
+        public function setCategory(){
+
+            $this -> category = $category;
+        }
+
+        public function getHtml(){
+
+            return $this -> getCategory();
+        }
+    }
+    $category1 = new Category ('Gatto');
+    $category2 = new Category ('Cane');
+    
+    $product = new Product ('Osso finto' , 180, 500, $category1);
 
     echo $product -> getHtml();
 ?>
