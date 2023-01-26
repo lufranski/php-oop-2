6907dae29bc5aadbbc58e7c62a5546fc80ba7b87
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-01-26 15:19:28 +0100

    Type class created

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
             return $this -> getName() . ' '
                     . $this -> getPrice() . ' euro '
                     . $this -> getWeight() . ' kg'
-                    . $this -> $category -> getHtml();
+                    . $this -> getCategory() -> getHtml();
         }
     }
 
@@ -75,20 +75,56 @@
 
             return $this -> category;
         }
-        public function setCategory(){
+        public function setCategory($category){
 
             $this -> category = $category;
         }
 
         public function getHtml(){
 
-            return $this -> getCategory();
+            return ' ' . $this -> getCategory();
         }
     }
+
+    class Type extends Product{
+
+        private $type;
+
+        public function __construct($name, $price, $weight, $category, $type){
+
+            parent :: __construct($name, $price, $weight, $category);
+
+            $this -> setType($type);
+        }
+
+        public function getType(){
+
+            return $this -> type;
+        }
+        public function setType($type){
+
+            $this -> type = $type;
+        }
+
+        public function getHtml(){
+
+            return parent :: getHtml() . ' '
+                    . $this -> getType();
+        }
+    }
+    
+    
     $category1 = new Category ('Gatto');
     $category2 = new Category ('Cane');
     
-    $product = new Product ('Osso finto' , 180, 500, $category1);
+    $product1 = new Product ('Osso finto' , 180, 500, $category1);
+    $product2 = new Product ('Cuccia' , 400, 400, $category2);
+    
+    $type1 = new Type ('Palla', 10, 0.2, $category2, 'Giochi');
+    $type2 = new Type ('Crocchette per gatti', 18, 0.500, $category1, 'Food');
 
-    echo $product -> getHtml();
+    echo $product1 -> getHtml() . '<br>';
+    echo $product2 -> getHtml();
+    echo '<br>' . $type1 -> getHtml();
+    echo '<br>' . $type2 -> getHtml();
 ?>
